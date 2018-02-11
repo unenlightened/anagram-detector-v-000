ba6bf62dff88231bb3735ff3801ab8e3f7966a6e
Chely Ho <adept_inept@yahoo.com>
2018-02-11 23:28:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram               #word or phrase formed by rearranging letters of another
+  attr_accessor :word, :phrase
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select {|word| word.split("").sort == @word.split("").sort}
+  end
+
+end
