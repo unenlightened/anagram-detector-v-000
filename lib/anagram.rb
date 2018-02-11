e41c381f856b6034e353264f5a040774d96f5284
Chely Ho <adept_inept@yahoo.com>
2018-02-11 23:28:51 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,6 @@
 # Your code goes here!
 class Anagram               #word or phrase formed by rearranging letters of another
-  attr_accessor :word, :phrase
+  attr_accessor :word
 
   def initialize(word)
     @word = word
